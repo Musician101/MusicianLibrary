c979c8dcd32cb01c99d9e5c18bc8caa2937978ff
Musician101 <yellowfireproductions@yahoo.com>
2015-10-21 09:54:39 -0700

    Add SpigotHelpCommand.

---
+++ src/main/java/musician101/common/java/minecraft/spigot/command/SpigotHelpCommand.java
@@ -0,0 +1,25 @@
+package musician101.common.java.minecraft.spigot.command;
+
+import java.util.Arrays;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class SpigotHelpCommand extends AbstractSpigotCommand
+{
+    private final AbstractSpigotCommand mainCommand;
+
+    public SpigotHelpCommand(AbstractSpigotCommand mainCommand)
+    {
+        super("help", "Display help info for " + ChatColor.stripColor(mainCommand.getUsage()), Arrays.asList(new SpigotCommandArgument(ChatColor.stripColor(mainCommand.getUsage())), new SpigotCommandArgument("help")), 1, "", false, "", "");
+        this.mainCommand = mainCommand;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, String... args)
+    {
+        sender.sendMessage(mainCommand.getCommandHelpInfo());
+        mainCommand.getSubCommands().forEach(command -> sender.sendMessage(command.getCommandHelpInfo()));
+        return true;
+    }
+}
