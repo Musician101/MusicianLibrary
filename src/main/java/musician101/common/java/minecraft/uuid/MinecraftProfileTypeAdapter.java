7b895804ab715e226982f25b2199789e835a71b3
Bruce <yellowfireproductions@yahoo.com>
2016-01-28 15:05:21 -0800

    Minecraft UUIDUtils!

---
+++ src/main/java/musician101/common/java/minecraft/uuid/MinecraftProfileTypeAdapter.java
@@ -0,0 +1,33 @@
+package musician101.common.java.minecraft.uuid;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+import java.util.UUID;
+import musician101.common.java.minecraft.uuid.UUIDUtils.MinecraftProfile;
+
+public class MinecraftProfileTypeAdapter implements JsonDeserializer<MinecraftProfile>, JsonSerializer<MinecraftProfile>
+{
+    Gson gson = new Gson();
+
+    @Override
+    public MinecraftProfile deserialize(JsonElement element, Type typeOfT, JsonDeserializationContext context)
+    {
+        JsonObject jo = element.getAsJsonObject();
+        String id = jo.get("id").getAsString();
+        UUID uuid = UUID.fromString(id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" + id.substring(20, 32));
+        String name = jo.get("name").getAsString();
+        return new MinecraftProfile(uuid, name);
+    }
+
+    @Override
+    public JsonElement serialize(MinecraftProfile profile, Type type, JsonSerializationContext context)
+    {
+        return null;
+    }
+}
