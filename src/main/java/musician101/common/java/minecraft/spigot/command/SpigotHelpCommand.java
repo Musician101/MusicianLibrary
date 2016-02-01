ba8c14bba19fb67614f8d3de24638643504ab5f9
Bruce <yellowfireproduction@yahoo.com>
2016-02-01 10:25:51 -0800

    Forgot to change this along with the last commit.

--- src/main/java/musician101/common/java/minecraft/spigot/command/SpigotHelpCommand.java
+++ src/main/java/musician101/common/java/minecraft/spigot/command/SpigotHelpCommand.java
@@ -3,14 +3,15 @@ package musician101.common.java.minecraft.spigot.command;
 import java.util.Arrays;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotHelpCommand extends AbstractSpigotCommand
+public class SpigotHelpCommand<Plugin extends JavaPlugin> extends AbstractSpigotCommand<Plugin>
 {
-    private final AbstractSpigotCommand mainCommand;
+    private final AbstractSpigotCommand<Plugin> mainCommand;
 
-    public SpigotHelpCommand(AbstractSpigotCommand mainCommand)
+    public SpigotHelpCommand(Plugin plugin, AbstractSpigotCommand<Plugin> mainCommand)
     {
-        super("help", "Display help info for " + ChatColor.stripColor(mainCommand.getUsage()), Arrays.asList(new SpigotCommandArgument(ChatColor.stripColor(mainCommand.getUsage())), new SpigotCommandArgument("help")), 1, "", false, "", "");
+        super(plugin, "help", "Display help info for " + ChatColor.stripColor(mainCommand.getUsage()), Arrays.asList(new SpigotCommandArgument(ChatColor.stripColor(mainCommand.getUsage())), new SpigotCommandArgument("help")), 1, "", false, "", "");
         this.mainCommand = mainCommand;
     }
 
