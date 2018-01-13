7b6d300ee0eaa2a9cf0e97ed5b3a47c1e73908ab
Bruce (Musician101) <bruceabivens@gmail.com>
2018-01-13 03:25:59 -0800

    Check if the PluginContainer contains an instance of the plugin.

--- musicianlibrary-sponge/src/main/java/io/musician101/musicianlibrary/java/minecraft/sponge/gui/anvil/SpongeJumpToPage.java
+++ musicianlibrary-sponge/src/main/java/io/musician101/musicianlibrary/java/minecraft/sponge/gui/anvil/SpongeJumpToPage.java
@@ -1,10 +1,13 @@
 package io.musician101.musicianlibrary.java.minecraft.sponge.gui.anvil;
 
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import javax.annotation.Nonnull;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 public class SpongeJumpToPage<P extends PluginContainer> extends SpongeAnvilGUI {
 
@@ -22,7 +25,15 @@ public class SpongeJumpToPage<P extends PluginContainer> extends SpongeAnvilGUI 
                 return "Page cannot exceed " + maxPage;
             }
 
-            Task.builder().delayTicks(1L).execute(() -> biConsumer.accept(player, page)).submit(plugin.getInstance().get());
+            Optional instance = plugin.getInstance();
+            if (instance.isPresent()) {
+                Task.builder().delayTicks(1L).execute(() -> biConsumer.accept(player, page)).submit(instance.get());
+            }
+            else {
+                player.closeInventory();
+                player.sendMessage(Text.builder(plugin.getId() + " does not have a valid instance. This GUI will not work until it does.").color(TextColors.RED).build());
+            }
+
             return null;
         });
     }
