924a28fdc53a661053abb77688fa5bbc8078f338
Bruce (Musician101) <bruceabivens@gmail.com>
2018-02-21 00:02:07 -0800

    Add a method to get the previous GUI.

--- musicianlibrary-common/src/main/java/io/musician101/musicianlibrary/java/minecraft/gui/chest/ChestGUI.java
+++ musicianlibrary-common/src/main/java/io/musician101/musicianlibrary/java/minecraft/gui/chest/ChestGUI.java
@@ -1,17 +1,24 @@
 package io.musician101.musicianlibrary.java.minecraft.gui.chest;
 
 import java.util.List;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public abstract class ChestGUI<C, G extends ChestGUI<C, G, I, J, P, S>, I, J, P, S> {
 
+    @Nonnull
     protected final List<GUIButton<C, G, I, J, P, S>> buttons;
+    @Nonnull
     protected final I inventory;
     protected final int page;
+    @Nonnull
     protected final P player;
+    @Nonnull
     protected final J plugin;
+    @Nullable
     protected final G prevGUI;
 
-    protected ChestGUI(I inventory, P player, int page, List<GUIButton<C, G, I, J, P, S>> buttons, G prevGUI, J plugin, boolean manualOpen) {
+    protected ChestGUI(@Nonnull I inventory, @Nonnull P player, int page, @Nonnull List<GUIButton<C, G, I, J, P, S>> buttons, @Nullable G prevGUI, @Nonnull J plugin, boolean manualOpen) {
         this.inventory = inventory;
         this.player = player;
         this.page = page;
@@ -24,6 +31,11 @@ public abstract class ChestGUI<C, G extends ChestGUI<C, G, I, J, P, S>, I, J, P,
     }
 
     public abstract void close();
+
+    @Nullable
+    public G getPreviousGUI() {
+        return prevGUI;
+    }
 
     public abstract void open();
 }
